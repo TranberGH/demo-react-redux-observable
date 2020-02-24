efd7f989a1277fd187fbaf0e462ca6f45c343e57
Bertrand JAN <pentrarto.jan@gmail.com>
2020-02-24 09:24:07 +0100

    add prettier and tslint

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import {
-  Search,
-  ResultList,
-} from '../../components';
+import { Search, ResultList } from "components";
 
-import './Home.scss';
+import "./Home.scss";
 
 interface HomeProps {
   search: Function;
@@ -20,18 +17,20 @@ interface HomeProps {
 function Home(props: HomeProps) {
   return (
     <>
-    <p className="top-block"><Link to="/regions">Rechercher par région et département</Link></p>
-    <div>
-      <Search search={props.search}/>
-      <ResultList
-        items={props.cities}
-        listClasses={['cities-list']}
-        itemClasses={['city-item']}
-        link={true}
-      />
-    </div>
+      <p className="top-block">
+        <Link to="/regions">Rechercher par région et département</Link>
+      </p>
+      <div>
+        <Search search={props.search} />
+        <ResultList
+          items={props.cities}
+          listClasses={["cities-list"]}
+          itemClasses={["city-item"]}
+          link={true}
+        />
+      </div>
     </>
-  )
+  );
 }
 
 export default Home;
--- src/store/epics/regions.ts
+++ src/store/epics/regions.ts
@@ -1,29 +1,26 @@
-import { from } from 'rxjs';
-import { fromFetch } from 'rxjs/fetch';
-import { map, mergeMap } from 'rxjs/operators';
-import { ofType } from 'redux-observable';
+import { from } from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+import { map, mergeMap } from "rxjs/operators";
+import { ofType } from "redux-observable";
 
-import {
-  FETCH_REGIONS,
-  fetchRegionsSuccess,
-} from '../actions';
-import { Api } from '../../core';
-import { Region } from '../../types';
+import { FETCH_REGIONS, fetchRegionsSuccess } from "../actions";
+import { Api } from "core";
+import { Region } from "../../types";
 
-const getAllRegionsEpic = (action$: any) => action$.pipe(
-  ofType(FETCH_REGIONS),
-  mergeMap(() => fromFetch(Api.getAllRegionsUrl()).pipe(
-      mergeMap(response => {
-        const resp: Promise<Region[]> = response.json()
-        return from(resp)
-      })
-    )
-  ),
-  map((regions: Region[]) => {
-    return fetchRegionsSuccess(regions)
-  })
-)
+const getAllRegionsEpic = (action$: any) =>
+  action$.pipe(
+    ofType(FETCH_REGIONS),
+    mergeMap(() =>
+      fromFetch(Api.getAllRegionsUrl()).pipe(
+        mergeMap(response => {
+          const resp: Promise<Region[]> = response.json();
+          return from(resp);
+        })
+      )
+    ),
+    map((regions: Region[]) => {
+      return fetchRegionsSuccess(regions);
+    })
+  );
 
-export {
-  getAllRegionsEpic,
-}
+export { getAllRegionsEpic };
